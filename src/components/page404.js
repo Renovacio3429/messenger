fa6e6139ba0b34afc1cf54c623b3403f019ae64e
Renovacio3429 <sav3429@gmail.com>
2022-10-13 08:19:48 +0700

    add project

---
+++ src/components/page404.js
@@ -0,0 +1,12 @@
+import Handlebars from 'handlebars';
+import errorData from '../template-data/error-data.json';
+import errorTemplate from './templates/error-page.tmpl';
+import titleTemplate from './templates/title.tmpl';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const titleCompiled = Handlebars.compile(titleTemplate);
+    title.innerHTML = titleCompiled(errorData.errorPageData.page404);
+
+    const errorCompiled = Handlebars.compile(errorTemplate);
+    app.innerHTML = errorCompiled(errorData.errorPageData.page404.data);
+});
