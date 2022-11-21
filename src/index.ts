a425771643c59cd7a1fc0273de4e47b84126e746
Renovacio3429 <sav3429@gmail.com>
2022-11-21 18:58:36 +0700

    test pre-commit3

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import Router from "core/Router/Router";
-import AuthController from "controllers/AuthController";
-import { LoginPage } from "pages/Login/LoginPage";
-import { ErrorPage } from "pages/ErrorPages/ErrorPage/ErrorPage";
-import { NotFoundPage } from "pages/ErrorPages/NotFoundPage/NotFoundPage";
-import { RoutesConfig } from "core/Router/RouterConfig";
-import { SignIn } from "pages/SignUp/SignUpPage";
-import { Profile } from "pages/Profile/ProfilePage/ProfilePage";
-import { ProfileChangeData } from "pages/Profile/ProfileChangeDataPage/ProfileChangeDataPage";
-import { ProfileChangePwd } from "pages/Profile/ProfileChangePwdPage/ProfileChangePwdPage";
-import { Chat } from "pages/Chat/ChatPage";
-import "styles/styles.css";
+import Router from 'core/Router/Router';
+import AuthController from 'controllers/AuthController';
+import { LoginPage } from 'pages/Login/LoginPage';
+import { ErrorPage } from 'pages/ErrorPages/ErrorPage/ErrorPage';
+import { NotFoundPage } from 'pages/ErrorPages/NotFoundPage/NotFoundPage';
+import { RoutesConfig } from 'core/Router/RouterConfig';
+import { SignIn } from 'pages/SignUp/SignUpPage';
+import { Profile } from 'pages/Profile/ProfilePage/ProfilePage';
+import { ProfileChangeData } from 'pages/Profile/ProfileChangeDataPage/ProfileChangeDataPage';
+import { ProfileChangePwd } from 'pages/Profile/ProfileChangePwdPage/ProfileChangePwdPage';
+import { Chat } from 'pages/Chat/ChatPage';
+import 'styles/styles.css';
 
-                            window.addEventListener("DOMContentLoaded", async () => {
-        Router.use(RoutesConfig.Login, LoginPage)
+window.addEventListener('DOMContentLoaded', async () => {
+    Router.use(RoutesConfig.Login, LoginPage)
         .use(RoutesConfig.SignUp, SignIn)
-            .use(RoutesConfig.Error, ErrorPage)
+        .use(RoutesConfig.Error, ErrorPage)
         .use(RoutesConfig.NotFound, NotFoundPage)
-         .use(RoutesConfig.Profile, Profile)
+        .use(RoutesConfig.Profile, Profile)
         .use(RoutesConfig.ChangeProfileData, ProfileChangeData)
         .use(RoutesConfig.ChangeProfilePwd, ProfileChangePwd)
         .use(RoutesConfig.Chat, Chat);
